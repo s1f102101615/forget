b6ce320ac3eb94b1569d3192f4ffd7700ffe0848
s1f102101615 <s1f102101615@iniad.org>
2023-09-19 23:03:00 +0900

    button

---
+++ client/src/pages/register/index.page.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css';
+
+const Home = () => {
+  return (
+    <>
+      <div>バーコードの画像を入力してください</div>
+      <button className={styles.btn}>BUTTON</button>
+    </>
+  );
+};
+
+export default Home;
